3e7cffacb68cc7122ee8da9f62628f103df5e671
alien <148527832+itsalien024@users.noreply.github.com>
2024-01-08 11:17:35 +0300

    Clicking on lists loads lisetup for titles and tasks

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let addBtn = document.getElementById("add-list");
+let allTasksBtn = document.getElementById("all-tasks");
+let tasksContainer = document.querySelector("#tasks-container");
+let newTaskBtn = document.querySelectorAll(".new-task-btn");
 
-function openCloseModal() {
+function openCloseListModal() {
   let openListModal = document.getElementById("open-modal");
   let modal = document.getElementById("list-modal");
   let closeModal = document.getElementById("close-list-modal");
@@ -13,7 +16,7 @@ function openCloseModal() {
     modal.style.display = "none";
   }
 }
-openCloseModal();
+openCloseListModal();
 
 function list(title) {
   return {title};
@@ -24,18 +27,24 @@ addBtn.onclick = function() {
   let listContainer = document.querySelector(".list-container");
   let listModal = document.getElementById("list-modal");
   let form = document.getElementById("new-list-form");
+  const LISTS = [];
 
   let newListLine = document.createElement("div");
+  newListLine.classList.add("newListLine")
   if (newTitle.trim() !== "") {
     newListLine.innerHTML = `
-    <div class="newList">${newTitle}<button class="listDeleteBtn">&times;</button></div>
+    <button class="newList">${newTitle}</button>
+    <button class="listDeleteBtn"><img src="images/delete.svg" alt=""></button>
     `
     listContainer.appendChild(newListLine);
+    LISTS.push(newTitle);
 
     let listDeleteBtns = document.querySelectorAll('.listDeleteBtn');
     listDeleteBtns.forEach(btn => {
       btn.addEventListener("click", function() {
         this.parentElement.remove();
+        showAllTasks();
+        allTasksBtn.click();
       })
     })
 
@@ -44,5 +53,57 @@ addBtn.onclick = function() {
   }else {
     alert("Please enter a valid List name");
   }
+
+  function clickList() {
+    let listTitle = document.querySelectorAll(".newList")
+    
+    listTitle.forEach((list) => {
+      list.addEventListener("click", function() {
+        tasksContainer.innerHTML = `
+        <div class="tasks-content">
+          <div class="task-list-title">
+            <div>${list.textContent}</div>
+          </div>
+          <div class="new-tasks-container">
+            
+          </div>
+          <button type="button" class="new-task-btn">+ Add Task</button>
+        </div>
+        `   
+      })
+    })
+  }
+  clickList();
 }
+
+function showAllTasks() {
+  allTasksBtn.onclick = function() {
+    tasksContainer.innerHTML = `
+      <div class="tasks-content">
+        <div class="task-list-title">
+          <div>${allTasksBtn.textContent}</div>
+        </div>
+        <div class="new-tasks-container">
+          
+        </div>
+        <button type="button" class="new-task-btn">+ Add Task</button>
+      </div>
+    `   
+  }
+}
+showAllTasks();
+
+function openCloseTaskModal() {
+  let newTaskModal = document.querySelector("#newTaskModal");
+
+  newTaskBtn.forEach(btn => {
+    btn.addEventListener("click", function() {
+      newTaskModal.style.display = "block";
+    })
+  })
+}
+openCloseTaskModal();
+
+
+
 
